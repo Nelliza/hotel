a59c55560bac59a0a69d551290b1a7357cd21fb6
Nelliza <nelliza.krd@gmail.com>
2019-02-06 22:07:41 +0300

    Реализовала блок Percentages с использованием jquery-плагинов

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ import $ from 'jquery';
 const jQuery = $;
 window.jQuery = $;
 window.$ = $;
+
+require('percircle');
+
+require('./vendor/Peity/jquery.peity.js');
+
+$(function(){
+  $('.percircle').percircle({
+    progressBarColor: '#e75735'
+  });
+
+  $('.pie-chart').peity('donut', {
+    fill: ['#747474', '#e75735', '#4eb7a8', '#e5e5e5'],
+    width: '7.5rem',
+    height: '7.5rem'
+  });
+});
