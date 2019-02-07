ce2cec02d9498a594d88ab1f589bbf835cb5a7d1
Nelliza <nelliza.krd@gmail.com>
2019-02-07 22:58:24 +0300

    Стилизовала и оживила блок с кнопками, в т.ч. ripple-эффект при клике

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ window.$ = $;
 
 require('percircle');
 
-require('./vendor/Peity/jquery.peity.js');
+require('./vendor/Material-Design-Style-Ripple-Effect-On-Click-Touch-Ripple-js/ripple.js');
+require('./vendor/Peity/jquery.peity.min.js');
 
 $(function(){
+
+  $('.btn').click(function() {
+    $(this).addClass('btn--pressed');
+  });
+
+  for (var i = 0; i < 4; i++) {
+    $('.btn').eq(i).trigger('click');
+  }
+
+  for (var i = 4; i < 8; i++) {
+    $('.btn').eq(i).trigger('hover');
+  }
+
   $('.percircle').percircle({
     progressBarColor: '#e75735'
   });
