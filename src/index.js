e707d8cffc2335fb2d22f711d571937a0ac47457
Nelliza <nelliza.krd@gmail.com>
2019-02-10 21:28:17 +0300

    Доделала блок Sliders

--- src/index.js
+++ src/index.js
@@ -28,14 +28,34 @@ $(function(){
     innerRadius: 47
   });
 
-  var handle1 = $('#handle1');
+  var pointer = $('.slider__pointer');
   $('#slider1').slider({
-    'value': 36,
+    value: 40,
     create: function() {
-      handle1.text($(this).slider('value'));
+      pointer.text($(this).slider('value'));
     },
     slide: function() {
-      handle1.text($(this).slider('value'));
+      pointer.text($(this).slider('value'));
+    }
+  });
+
+  $('#slider2').slider({
+    value: 75,
+    step: 25,
+    range: 'min',
+    create: function() {
+      var ul = $('.slider__scale');
+      var count = $(this).slider('option', 'max') / $(this).slider('option', 'step') + 1;
+      var step = 0;
+      for (var i = 0; i < count; i++) {
+        ul.append($('<li>', {
+          class: 'slider__step',
+          text: step,
+          style: 'left: ' + step + '%'
+        }));
+        step += $(this).slider('option', 'step');
+      }
+      ul.appendTo('#slider2');
     }
   });
 });
