b4c0d59ee49a643791e6280320f73be4f3bf184e
Nelliza <nelliza.krd@gmail.com>
2019-02-27 20:31:07 +0300

    Стилизовала блок Calendar, добавила функциональность

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -8,16 +8,21 @@ $(function() {
     dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
     prevText: '',
     nextText: '',
-    onSelect: selectedDay
+    onSelect: selectedDay,
+    onChangeMonthYear: cleanDay
   });
 
   function currentDay() {
-    var dayText = $.datepicker.formatDate('d', new Date());
+    var dayText = $.datepicker.formatDate('dd', new Date());
     $('.calendar__day').text(dayText);
   }
 
   function selectedDay() {
-    var dayText = $.datepicker.formatDate('d', $(this).datepicker('getDate'));
+    var dayText = $.datepicker.formatDate('dd', $(this).datepicker('getDate'));
     $('.calendar__day').text(dayText);
+  }
+
+  function cleanDay() {
+    $('.calendar__day').text('');
   }
 });
