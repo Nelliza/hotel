3215a52fb8f76ea9348c39d2597d9d482ae903b4
Nelliza <nelliza.krd@gmail.com>
2019-03-23 21:20:51 +0300

    Выделила цветом активную ссылку в главном меню сайта

--- src/pages/components/header/main-menu/main-menu.js
+++ src/pages/components/header/main-menu/main-menu.js
@@ -2,4 +2,13 @@ $(function() {
   $('.main-menu__btn').click(function() {
     $(this).closest('.main-menu').toggleClass('main-menu--open');
   });
+
+  var $link = $('.main-menu__link');
+  var url = document.location.href;
+
+  for(var i = 0; i < $link.length; i++) {
+    if (url == $link.get(i).href) {
+      $link.eq(i).addClass('main-menu__link--active');
+    }
+  }
 });
