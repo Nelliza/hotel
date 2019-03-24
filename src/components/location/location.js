9b9b49938f3288d9b91cf42309da68b313cbad71
Nelliza <nelliza.krd@gmail.com>
2019-03-24 14:47:01 +0300

    Пофиксила ошибку, возникающую при загрузке страницы, не содержащей Яндекс-карту

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -1,32 +1,34 @@
-ymaps.ready(init);
+(function() {
+  function init() {
+    var myMap = new ymaps.Map('location', {
+        center: [37.7975, -122.414483],
+        zoom: 14
+      }, {
+        searchControlProvider: 'yandex#search'
+      });
 
-function init() { 
-  var myMap = new ymaps.Map('location', {
-      center: [37.7975, -122.414483],
-      zoom: 14
-    }, {
-      searchControlProvider: 'yandex#search'
-    });
+      myBalloonLayout = ymaps.templateLayoutFactory.createClass(
+        '<span class="location__text location__text--greeting">Meet us!</span>' +
+        '<span class="location__text location__text--address">1259 California St<br>San Francisco, CA</span>' +
+        '<ul class="location__marks"><li class="location__mark location__mark--location"></li><li class="location__mark location__mark--pin"></li></ul>'
+      );
 
-    myBalloonLayout = ymaps.templateLayoutFactory.createClass(
-      '<span class="location__text location__text--greeting">Meet us!</span>' +
-      '<span class="location__text location__text--address">1259 California St<br>San Francisco, CA</span>' +
-      '<ul class="location__marks"><li class="location__mark location__mark--location"></li><li class="location__mark location__mark--pin"></li></ul>'
-    );
+    ymaps.layout.storage.add('my#balloonlayout', myBalloonLayout);
 
-  ymaps.layout.storage.add('my#balloonlayout', myBalloonLayout);
+    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        balloonContentLayout:'my#balloonlayout',
+        balloonPanelMaxMapArea: Infinity,
+        balloonCloseButton: false,
+        iconLayout: 'default#image',
+        iconImageHref: require('../_icons/marker-icon.svg'),
+        iconImageSize: [60, 80],
+        iconImageOffset: [-20, -34]
+      });
 
-  var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
-      balloonContentLayout:'my#balloonlayout',
-      balloonPanelMaxMapArea: Infinity,
-      balloonCloseButton: false,
-      iconLayout: 'default#image',
-      iconImageHref: require('../_icons/marker-icon.svg'),
-      iconImageSize: [60, 80],
-      iconImageOffset: [-20, -34]
-    });
+    myMap.geoObjects.add(myPlacemark);
 
-  myMap.geoObjects.add(myPlacemark);
+    myPlacemark.balloon.open();
+  }
 
-  myPlacemark.balloon.open();
-}
+  window.init = init;
+})();
