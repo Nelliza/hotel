393b794828b1f32aad7a8cd88933fe83e46915ba
Nelliza <nelliza.krd@gmail.com>
2019-03-28 08:47:55 +0300

    Вынесла main-menu в отдельный компонент без привязки к хедеру

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,7 +1,7 @@
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel';
 
-import './components/header/main-menu/main-menu.js';
+import './components/main-menu/main-menu.js';
 import './components/promo/promo.js';
 import './components/callback/callback.js';
 import './components/reservation/reservation.js';
