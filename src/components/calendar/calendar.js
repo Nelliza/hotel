058e968cc3abcb7926eeb99b12b183a160da1f15
Nelliza <nelliza.krd@gmail.com>
2019-06-23 14:13:12 +0300

    Исправила на календаре выбор сегодняшней даты, подправила стили стрелок календаря и бегунка слайдера

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -8,9 +8,30 @@ $(function() {
     dayNamesMin: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
     prevText: '',
     nextText: '',
-    onSelect: selectedDay,
-    onChangeMonthYear: cleanDay
+    onSelect: selectedDay
   });
+
+  $.datepicker._gotoToday = function(id) {
+    var date,
+      target = $(id),
+      inst = this._getInst(target[0]);
+
+    if (this._get(inst, "gotoCurrent") && inst.currentDay) {
+      inst.selectedDay = inst.currentDay;
+      inst.drawMonth = inst.selectedMonth = inst.currentMonth;
+      inst.drawYear = inst.selectedYear = inst.currentYear;
+    } else {
+      date = new Date();
+      inst.selectedDay = date.getDate();
+      inst.drawMonth = inst.selectedMonth = date.getMonth();
+      inst.drawYear = inst.selectedYear = date.getFullYear();
+      this._setDateDatepicker(target, date);
+      this._selectDate(id, this._getDateDatepicker(target));
+    }
+
+    this._notifyChange(inst);
+    this._adjustDate(target);
+  }
 
   function currentDay() {
     var dayText = $.datepicker.formatDate('dd', new Date());
@@ -20,9 +41,5 @@ $(function() {
   function selectedDay() {
     var dayText = $.datepicker.formatDate('dd', $(this).datepicker('getDate'));
     $('.calendar__day').text(dayText);
-  }
-
-  function cleanDay() {
-    $('.calendar__day').text('');
   }
 });
