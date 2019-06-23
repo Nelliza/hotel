c69aa5d84bef840f7a9e490114201943acf49b2f
Nelliza <nelliza.krd@gmail.com>
2019-06-23 10:09:15 +0300

    Заменила ссылки с href="#" в location.js

--- src/components/location/location.js
+++ src/components/location/location.js
@@ -10,7 +10,7 @@
       myBalloonLayout = ymaps.templateLayoutFactory.createClass(
         '<span class="location__text location__text--greeting">Meet us!</span>' +
         '<span class="location__text location__text--address">1259 California St<br>San Francisco, CA</span>' +
-        '<ul class="location__marks"><li class="location__mark location__mark--location"><a href="#"></a></li><li class="location__mark location__mark--pin"><a href="#"></a></li></ul>'
+        '<ul class="location__marks"><li class="location__mark location__mark--location"><a href="./handler.php"></a></li><li class="location__mark location__mark--pin"><a href="./handler.php"></a></li></ul>'
       );
 
     ymaps.layout.storage.add('my#balloonlayout', myBalloonLayout);
