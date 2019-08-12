6ffe4dff246a2d1394cc6ef0a6488f64574af19c
Nelliza <nelliza.krd@gmail.com>
2019-08-12 14:19:29 +0300

    Рефакторинг валидации формы #17

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -6,39 +6,43 @@ class Form {
   }
 
   init() {
+    this.$field = this.$form.find('.js-form__input');
+    this.$field.on(`focusout.field${this.index}`, this._handleFieldFocusout.bind(this));
     this.$form.on(`submit.form${this.index}`, this._handleFormSubmit.bind(this));
   }
 
+  _handleFieldFocusout(event) {
+    const $checkedField = $(event.currentTarget);
+    this._validateField($checkedField);
+  }
+
+  _validateField($checkedField) {
+    const fieldName = $checkedField.attr('name');
+    let regexp;
+
+    if (fieldName === 'login') {
+      regexp = /^[a-zA-Z][a-zA-Z0-9-_\.]{2,20}$/i;
+    } else if (fieldName === 'email') {
+      regexp = /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.([a-z]{2}|(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum))$/i;
+    }
+
+    this._checkField($checkedField, regexp);
+  }
+
+  _checkField($checkedField, regexp) {
+    if (regexp.test($checkedField.val())) {
+      $checkedField.next().attr('class', 'form__tooltip js-form__tooltip form__tooltip_type_correct').text('Thanks!');
+    } else {
+      $checkedField.next().attr('class', 'form__tooltip js-form__tooltip form__tooltip_type_error').text('Error');
+    }
+  }
+
   _handleFormSubmit(event) {
-    event.preventDefault();
-    this._validateForm();
-  }
+    const $tooltip = this.$form.find('.js-form__tooltip');
 
-  _validateForm() {
-    const $formVal = this.$form.find('.js-form__val');
-    $formVal.removeClass().text('');
-
-    const $loginField = this.$form.find('input[name="name"]');
-
-    if (this._checkLoginEntry($loginField.val())) {
-      $loginField.next().addClass('form__val js-form__val form__val_type_correct').text('Thanks!');
-    } else {
-      $loginField.next().addClass('form__val js-form__val form__val_type_error').text('Error');
+    if ($tooltip.hasClass('form__tooltip_type_error')) {
+      event.preventDefault();
     }
-
-    const regexp = /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.([a-z]{2}|(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum))$/i;
-    const emailField = this.$form.find('input[name="email"]');
-    const email = !emailField.val() || !regexp.test(emailField.val());
-
-    if (email) {
-      emailField.next().addClass('form__val js-form__val form__val_type_error').text('Error');
-    } else {
-      emailField.next().addClass('form__val js-form__val form__val_type_correct').text('Thanks!');
-    }
-  }
-
-  _checkLoginEntry(value) {
-    return (value.indexOf(' ') === -1) && (value.length >= 3);
   }
 }
 
