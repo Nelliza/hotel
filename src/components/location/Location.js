23f55b72fb4c088888a9909eba23301b35347d51
Nelliza <nelliza.krd@gmail.com>
2019-08-13 22:20:05 +0300

    Отрефакторила код класса Location

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,10 +1,14 @@
 class Location {
   constructor(myMap) {
-    this.createBalloon();
-    this.createPlacemark(myMap);
+    this.init(myMap);
   }
 
-  createBalloon() {
+  init(myMap) {
+    this._createBalloon();
+    this._createPlacemark(myMap);
+  }
+
+  _createBalloon() {
     const myBalloonLayout = ymaps.templateLayoutFactory.createClass(
       '<span class="location__text location__text_type_greeting">Meet us!</span><span class="location__text location__text_type_address">1259 California St<br>San Francisco, CA</span><ul class="location__marks"><li class="location__mark location__mark_type_location"><a class="location__mark-link" href="./handler.php"></a></li><li class="location__mark location__mark_type_pin"><a class="location__mark-link" href="./handler.php"></a></li></ul>',
     );
@@ -12,7 +16,7 @@ class Location {
     ymaps.layout.storage.add('my#balloonlayout', myBalloonLayout);
   }
 
-  createPlacemark(myMap) {
+  _createPlacemark(myMap) {
     const myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
       balloonContentLayout: 'my#balloonlayout',
       balloonPanelMaxMapArea: Infinity,
